20a5c8808b179ddca3657cef60de7e99d903c8b1
Takahiro <s1521035rg@s.chibakoudai.jp>
2018-09-12 18:04:07 +0900

    readActVel関数にインデックスのアドレスを追加

--- main.cpp
+++ main.cpp
@@ -257,10 +257,11 @@ void readActVel(int nodeID){
     canmsgTx.id = 0x600+nodeID;
     canmsgTx.len = 4;       //Data Length
     canmsgTx.data[0] = 0x40;//|0Byte:40|
-    canmsgTx.data[1] = 0x;//Index LowByte
-    canmsgTx.data[2] = 0x;//Index HighByte
+    canmsgTx.data[1] = 0x6C;//Index LowByte
+    canmsgTx.data[2] = 0x60;//Index HighByte
     canmsgTx.data[3] = 0x00;//sub-Index
     canPort.write(canmsgTx);
+    
 }
 
 //送信データの表示
